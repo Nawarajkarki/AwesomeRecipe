5db452175aea7e8947df77fbe1784aae4d5b9b39
Nawaraj Karki <nawarajkarkee404@gmail.com>
2023-12-11 22:14:26 +0545

    change back to postgres

--- AwesomeRecipe/settings.py
+++ AwesomeRecipe/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -66,23 +69,24 @@ WSGI_APPLICATION = "AwesomeRecipe.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'awesomerecipe',
-#         'USER': 'postgres',
-#         'PASSWORD': 'nope',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME' : os.environ.get("DB_NAME"),
+        'USER' : os.environ.get("USER"),
+        'PASSWORD' : os.environ.get("PASSWORD"),
+        'HOST' : os.environ.get("HOST"),
+        'PORT' : os.environ.get("PORT"),
+        
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
